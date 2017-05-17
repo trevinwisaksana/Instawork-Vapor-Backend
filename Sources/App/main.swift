8410af836969a90410f2cddc858965557109301e
Trevin Wisaksana <trevinwisaksana@gmail.com>
2017-05-17 11:22:43 +0700

    • Experimented with leaf templating

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -7,11 +7,45 @@ drop.preparations += Instawork.self
 
 
 do {
+    // Adding the PostgreSQL provider
     try drop.addProvider(VaporPostgreSQL.Provider.self)
 } catch {
     assertionFailure("Error adding provider: \(error)")
 }
 
+// MARK: Testing leaf templating
+drop.get("template1") { (request) in
+    return try drop.view.make("hello", Node(node: ["name" : "Trevin"]))
+}
+
+drop.get("template2", String.self) { (request, name) in
+    return try drop.view.make("hello", Node(node: ["name" : name]))
+}
+
+drop.get("template3") { (request) in
+    let users = try ["Trevin", "Amalie", "Wisaksana"].makeNode()
+    return try drop.view.make("loop", Node(node: ["users" : users]))
+}
+
+drop.get("template4") { (request) in
+    let users = try [
+        ["name" : "Trevin", "email" : "trevin@trevin.com"].makeNode(),
+        ["name" : "Trevin", "email" : "trevin@trevin.com"].makeNode(),
+        ["name" : "Trevin", "email" : "trevin@trevin.com"].makeNode()
+        ].makeNode()
+    return try drop.view.make("email", Node(node: ["users" : users]))
+}
+
+
+drop.get("template5") { (request) in
+    guard let sayHello = request.data["sayHello"]?.bool else {
+        throw Abort.badRequest
+    }
+    return try drop.view.make("if", Node(node: ["sayHello" : sayHello.makeNode()]))
+}
+
+
+// MARK: Instawork's backend
 drop.get { req in
     return try drop.view.make("welcome", [
         "message": drop.localization[req.lang, "welcome", "title"]
